0e02052e8d8233ceaab3758c435ab4b0d32f5681
ktylak <karolina.tylak@student.uw.edu.pl>
2017-03-08 16:34:30 +0100

    Add files via upload

---
+++ grupa1/ktylak/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Point{
+
+    double x,y;
+
+public:
+
+    Point(doube _x, double _y) {
+        x=_x; y=_y;
+
+    }
+
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
