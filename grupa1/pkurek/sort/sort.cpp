86e7edc0b312d06453b446fc6e3c369b0803f01d
cure <przemyslaw.kurek85@gmail.com>
2017-04-19 16:04:35 +0200

    added sort.cpp

---
+++ grupa1/pkurek/sort/sort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <chrono>
+#include <cstdio>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int c = a; a = b; b = c;
+}
+
+// sortuje t[p] .. t[k - 1] - bez t[k] !
+void bubble_sort(int t[], int p, int k)
+{
+    // DO ZAIMPLEMENTOWANIA
+}
+
+// sortuje t[p] .. t[k - 1] - bez t[k] !
+void merge_sort(int t[], int p, int k)
+{
+    // DO ZAIMPLEMENTOWANIA
+}
+
+void test(int n)
+{
+    int t1[n], t2[n];
+    
+    for (int i = 0; i < n; i++) {
+        t1[i] = t2[i] = rand();
+    }
+
+    cout << "Table size: " << n << endl;
+
+    auto start = chrono::steady_clock::now();
+    bubble_sort(t1, 0, n);
+    auto end = chrono::steady_clock::now();
+
+    auto diff_bubble = end - start;
+
+    start = chrono::steady_clock::now();
+    merge_sort(t2, 0, n);
+    end = chrono::steady_clock::now();
+
+    auto diff_merge = end - start;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (t1[i] > t1[i + 1]) {
+            cout << "  Wrong answer - bubble sort." << endl; break;
+        }
+        if (t2[i] > t2[i + 1]) {
+            cout << "  Wrong answer - merge sort." << endl; break;
+        }
+    }
+    
+    cout << "  Bubble sort time: " << chrono::duration <double> (diff_bubble).count() << endl;
+    cout << "  Merge sort time: " << chrono::duration <double> (diff_merge).count() << endl;
+    cout << "  Ratio: " << (chrono::duration <double> (diff_bubble).count() / chrono::duration <double> (diff_merge).count()) << endl;
+}
+
+int main()
+{
+    test(100);
+    test(1000);
+    test(10000);
+    test(20000);
+    test(30000);
+    test(50000);
+    test(100000);
+}
