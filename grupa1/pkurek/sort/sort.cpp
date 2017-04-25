ac8054a124b9ca7d77f1647504124e9cfe91dcb8
cure <przemyslaw.kurek85@gmail.com>
2017-04-25 14:37:47 +0200

    added sort.cpp

--- grupa1/pkurek/sort/sort.cpp
+++ grupa1/pkurek/sort/sort.cpp
@@ -11,13 +11,127 @@ void swap(int &a, int &b)
 // sortuje t[p] .. t[k - 1] - bez t[k] !
 void bubble_sort(int t[], int p, int k)
 {
-    // DO ZAIMPLEMENTOWANIA
+    /*
+    //bubble sort: loops n^2 times
+    for(int j = p; j < k - 1; j++) {
+        for(int i = p; i < k - 1; i++) {
+            if(t[i] > t[i + 1]) {swap(t[i], t[i + 1]);}
+        }
+    }
+    */
+
+    /*
+    //bubble sort: remembers if did an action - stops if no action is taken
+    bool swapped = true;
+    while(swapped) {
+    swapped = false;
+        for(int i = p; i < k - 1; i++) {
+            if(t[i] > t[i + 1]) {swap(t[i], t[i + 1]); swapped = true;}
+        }
+    }
+    */
+
+    /*
+    //bubble sort: assumes last element is on its position
+    int sunk = 0; //number of sunken elements
+    for(int j = p; j < k - 1; j++) {
+        for(int i = p; i < k - 1 - sunk; i++) {
+            if(t[i] > t[i + 1]) {swap(t[i], t[i + 1]);}
+        }
+        sunk++;
+    }
+    */
+
+    //bubble sort: remembers last swap position, 
+    //then calculates number of sunk elements
+    //number of sunk elements
+    while(k > 1){
+        int last_swap = p;
+        for(int i = p; i < k - 1; i++) {
+            if(t[i] > t[i + 1]) {swap(t[i], t[i + 1]);last_swap = i;}
+        }
+        k = last_swap + 1;
+    }
 }
 
 // sortuje t[p] .. t[k - 1] - bez t[k] !
+
+bool is_sorted(int t[], int p, int k) {
+    for(int i = p; i < k-1; i++) {
+        if(t[i] > t[i+1]) {
+            //cout << "unsorted: " << p << ":" << k << endl;
+            return false;
+        }
+    }
+    //cout << "sorted: " << p << ":" << k << endl;
+    return true;
+}
+
+void do_merge(int t[], int p, int half, int k) {
+    //cout << "do_merge init" << p << k << endl;
+    int length = k - p;
+    if(length == 1) {return;}
+    int length1 = length / 2;
+    int length2 = length - length1;
+
+    int* tmp1 = new int[length1];
+    int* tmp2 = new int[length2];
+
+    for(int i = 0; i < length1 ; i++) {
+        tmp1[i] = t[p + i];
+        //cout << "part1" << endl;
+        //cout << i << tmp1[i] << endl;
+    }
+
+    for(int i = 0; i < length2; i++) {
+        tmp2[i] = t[half + i];
+        //cout << "part2" << endl;
+        //cout << i << tmp2[i] << endl;
+    }
+    
+    int t1 = 0;
+    int t2 = 0;
+    //cout << "sorted_part: ";
+    for(int i = p; i < k; i++) {
+        if(t1==length1){
+            //cout << "part1empty" << endl;
+            t[i] = tmp2[t2];
+            //cout << tmp2[t2] << " ";
+            t2++;
+            continue;
+        }
+        if(t2==length2){
+            //cout << "part2empty" << endl;
+            t[i] = tmp1[t1];
+            //cout << tmp1[t1] << " ";
+            t1++;
+            continue;
+        }
+        if(tmp1[t1] <= tmp2[t2]) {
+            t[i] = tmp1[t1];
+            //cout << tmp1[t1] << " ";
+            t1++;
+        } else {
+            t[i] = tmp2[t2];
+            //cout << tmp2[t2] << " ";
+            t2++;
+        }
+    }
+    //cout << endl;
+    delete[] tmp1;
+    delete[] tmp2;
+}
+
 void merge_sort(int t[], int p, int k)
 {
-    // DO ZAIMPLEMENTOWANIA
+    if(!is_sorted(t, p, k)) {
+        int half = p + (k-p) / 2;
+        if(p != half) {
+            merge_sort(t, p, half);
+            merge_sort(t, half, k);
+        }
+        do_merge(t, p, half, k);
+    }
 }
 
 void test(int n)
@@ -26,6 +140,7 @@ void test(int n)
     
     for (int i = 0; i < n; i++) {
         t1[i] = t2[i] = rand();
+        //cout << t1[i] << endl;
     }
 
     cout << "Table size: " << n << endl;
@@ -51,13 +166,19 @@ void test(int n)
         }
     }
     
-    cout << "  Bubble sort time: " << chrono::duration <double> (diff_bubble).count() << endl;
-    cout << "  Merge sort time: " << chrono::duration <double> (diff_merge).count() << endl;
-    cout << "  Ratio: " << (chrono::duration <double> (diff_bubble).count() / chrono::duration <double> (diff_merge).count()) << endl;
+    cout << "  Bubble sort time: " 
+         << chrono::duration <double> (diff_bubble).count() 
+         << endl;
+    cout << "  Merge sort time: " 
+         << chrono::duration <double> (diff_merge).count() 
+         << endl;
+    cout << "  Ratio: " 
+         << (chrono::duration <double> (diff_bubble).count() / chrono::duration <double> (diff_merge).count()) 
+         << endl;
 }
 
 int main()
-{
+{   test(60);
     test(100);
     test(1000);
     test(10000);
@@ -65,4 +186,5 @@ int main()
     test(30000);
     test(50000);
     test(100000);
+    
 }
